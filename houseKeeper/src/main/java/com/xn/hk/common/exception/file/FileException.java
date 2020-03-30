a6c0168b1431724239e95dff129b90628c5850d7
wanlei <wanl@koal.com>
2020-03-30 13:52:39 +0800

    添加公用异常类和spring相关的工具类，树形工具类，反射工具类，beanUtil工具类，完善字符串工具类

---
+++ houseKeeper/src/main/java/com/xn/hk/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package com.xn.hk.common.exception.file;
+
+import com.xn.hk.common.exception.file.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
--- houseKeeper/src/main/java/com/xn/hk/common/utils/string/StringUtil.java
+++ houseKeeper/src/main/java/com/xn/hk/common/utils/string/StringUtil.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.xn.hk.common.constant.Constant;
+import com.xn.hk.common.utils.text.StrFormatter;
 
 /**
  * @Title: StringUtil
@@ -34,6 +35,8 @@ import com.xn.hk.common.constant.Constant;
  */
 public final class StringUtil {
 	private static final Logger logger = LoggerFactory.getLogger(StringUtil.class);
+	/** 下划线 */
+	private static final char SEPARATOR = '_';
 	/**
 	 * 所有字符和数字
 	 */
@@ -63,10 +66,127 @@ public final class StringUtil {
 	}
 
 	/**
+	 * 下划线转驼峰命名
+	 */
+	public static String toUnderScoreCase(String str) {
+		if (str == null) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder();
+		// 前置字符是否大写
+		boolean preCharIsUpperCase = true;
+		// 当前字符是否大写
+		boolean curreCharIsUpperCase = true;
+		// 下一字符是否大写
+		boolean nexteCharIsUpperCase = true;
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (i > 0) {
+				preCharIsUpperCase = Character.isUpperCase(str.charAt(i - 1));
+			} else {
+				preCharIsUpperCase = false;
+			}
+
+			curreCharIsUpperCase = Character.isUpperCase(c);
+
+			if (i < (str.length() - 1)) {
+				nexteCharIsUpperCase = Character.isUpperCase(str.charAt(i + 1));
+			}
+
+			if (preCharIsUpperCase && curreCharIsUpperCase && !nexteCharIsUpperCase) {
+				sb.append(SEPARATOR);
+			} else if ((i != 0 && !preCharIsUpperCase) && curreCharIsUpperCase) {
+				sb.append(SEPARATOR);
+			}
+			sb.append(Character.toLowerCase(c));
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * 将下划线大写方式命名的字符串转换为驼峰式。如果转换前的下划线大写方式命名的字符串为空，则返回空字符串。
+	 * 例如：HELLO_WORLD->HelloWorld
+	 * 
+	 * @param name
+	 *            转换前的下划线大写方式命名的字符串
+	 * @return 转换后的驼峰式命名的字符串
+	 */
+	public static String convertToCamelCase(String name) {
+		StringBuilder result = new StringBuilder();
+		// 快速检查
+		if (name == null || name.isEmpty()) {
+			// 没必要转换
+			return "";
+		} else if (!name.contains("_")) {
+			// 不含下划线，仅将首字母大写
+			return name.substring(0, 1).toUpperCase() + name.substring(1);
+		}
+		// 用下划线将原始字符串分割
+		String[] camels = name.split("_");
+		for (String camel : camels) {
+			// 跳过原始字符串中开头、结尾的下换线或双重下划线
+			if (camel.isEmpty()) {
+				continue;
+			}
+			// 首字母大写
+			result.append(camel.substring(0, 1).toUpperCase());
+			result.append(camel.substring(1).toLowerCase());
+		}
+		return result.toString();
+	}
+
+	/**
+	 * 驼峰式命名法 例如：user_name->userName
+	 */
+	public static String toCamelCase(String s) {
+		if (s == null) {
+			return null;
+		}
+		s = s.toLowerCase();
+		StringBuilder sb = new StringBuilder(s.length());
+		boolean upperCase = false;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (c == SEPARATOR) {
+				upperCase = true;
+			} else if (upperCase) {
+				sb.append(Character.toUpperCase(c));
+				upperCase = false;
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
 	 * 去空格
 	 */
 	public static String trim(String str) {
 		return (str == null ? "" : str.trim());
+	}
+
+	/**
+	 * 格式化文本, {} 表示占位符<br>
+	 * 此方法只是简单将占位符 {} 按照顺序替换为参数<br>
+	 * 如果想输出 {} 使用 \\转义 { 即可，如果想输出 {} 之前的 \ 使用双转义符 \\\\ 即可<br>
+	 * 例：<br>
+	 * 通常使用：format("this is {} for {}", "a", "b") -> this is a for b<br>
+	 * 转义{}： format("this is \\{} for {}", "a", "b") -> this is \{} for a<br>
+	 * 转义\： format("this is \\\\{} for {}", "a", "b") -> this is \a for b<br>
+	 * 
+	 * @param template
+	 *            文本模板，被替换的部分用 {} 表示
+	 * @param params
+	 *            参数值
+	 * @return 格式化后的文本
+	 */
+	public static String format(String template, Object... params) {
+		if (isEmpty(params) || isEmpty(template)) {
+			return template;
+		}
+		return StrFormatter.format(template, params);
 	}
 
 	/**
